7a71a28d167c94120492addad7c3acc5aa3fcfe2
ytyaru <ytyaru@outlook.jp>
2021-11-12 11:07:02 +0900

    5_lib_builtin/18_IO

--- src/5_lib_builtin/18_IO/0.rb
+++ src/5_lib_builtin/18_IO/0.rb
@@ -12,6 +12,8 @@ IO.write("testfile2", "")
 f = File.new("testfile2", 'r+')
 f.puts 'putsのテスト出力。'
 f.puts '二行目。'
-f.flush
-f.fsync
-f.each { |line| p "#{f.lineno}: #{line}" } # なぜか出力されない
+#f.flush
+#f.fsync
+f.each { |line| p "#{f.lineno}: #{line}" } # 位置が末尾のため何も出力されない
+f.seek IO::SEEK_SET                        # 位置を先頭に戻す
+f.each { |line| p "#{f.lineno}: #{line}" } # 出力される
