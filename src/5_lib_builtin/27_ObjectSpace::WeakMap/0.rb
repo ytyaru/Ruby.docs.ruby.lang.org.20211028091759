7ffda203cbf0388b0e91b3e2d2c180e508fa85b7
ytyaru <ytyaru@outlook.jp>
2021-11-23 15:01:06 +0900

    5_lib_builtin/27_ObjectSpace::WeakMap

--- src/5_lib_builtin/27_ObjectSpace::WeakMap/0.rb
+++ src/5_lib_builtin/27_ObjectSpace::WeakMap/0.rb
@@ -1,24 +1,7 @@
 #!/usr/bin/env ruby
-
-# Float: 浮動少数 IEEE754
-# Rational: 有理数（x/yなど分数で表せる数）
-f = 0.1
-p f
-p f.class
-
-c = 2 / 3
-p c
-p c.class
-
-c = 2.0 / 3.0
-p c
-p c.class
-
-c = Rational(2, 3)
-p c
-p c.class
-p c.denominator # 分母
-p c.numerator   # 分子
-#c.denominator = 5 # undefined method `denominator=' for (2/3):Rational (NoMethodError)
-#c.numerator = 5   # undefined method `numerator=' for (2/3):Rational (NoMethodError)
-
+weak_map = ObjectSpace::WeakMap.new
+key = "text"
+p key.object_id #=> 36
+weak_map[key] = "test" # => test
+p weak_map[key] # => test
+p weak_map[key].object_id #=> 48
